d2c7b4dc1d8dcd4f44cea1717cec0179d60c78e2
unknown <mayank.kumar@ebix.com>
2024-06-30 23:58:58 +0530

    hi

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const IgAPI = require('instagram-graph-api');
+require('dotenv').config
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(3000, () => {
+    console.log('connected to port 3000');
+})
+
+
+const getToken = async(req,res) =>{
+    
+}
+
+const loginIG = async (req, res) => {
+    try {
+        const { USERNAME, PASSWORD } = process.env
+        const client = new IgAPI({ USERNAME, PASSWORD });
+        await client.login()
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json(error.message);
+    }
+
+}
+app.get('/', loginIG);
